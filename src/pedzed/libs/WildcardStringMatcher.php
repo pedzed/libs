0c0ab85d4455fa5ba99c0f8a3b4a8f8739c1a320
pedzed <pedzed@outlook.com>
2015-01-18 13:58:52 +0100

    Added WildcardStringMatcher.

---
+++ src/pedzed/libs/WildcardStringMatcher.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace pedzed\libs {
+    
+    /*
+    |--------------------------------------------------------------------------
+    | String matcher
+    |--------------------------------------------------------------------------
+    | 
+    | Checks if two strings match each other.
+    | 
+    */
+    class WildcardStringMatcher {
+        
+        /**
+         * Checks if two strings match each other.
+         * 
+         * @param  string $str1 The string to match.
+         * @param  string $str2 The string to match.
+         * 
+         * @return bool
+         */
+        public static function match($str1, $str2){
+            if(self::_hasWildcard($str1) || self::_hasWildcard($str2)){
+                if(self::_hasWildcard($str1)){
+                    $wildcardStr = $str1;
+                    $normalStr = $str2;
+                } else if(self::_hasWildcard($str2)){
+                    $wildcardStr = $str2;
+                    $normalStr = $str1;
+                }
+                
+                $pattern = preg_quote($wildcardStr, '#');
+                $pattern = str_replace('\*', '.*?', $pattern);
+                $pattern = '#^'.$pattern.'$#';
+                
+                return (preg_match($pattern, $normalStr) == true);
+            } else {
+                return $str1 === $str2;
+            }
+        }
+        
+        /**
+         * Tells whether the string has the wildcard character or not.
+         * 
+         * @param  string $str The string to check.
+         * @return bool
+         */
+        protected static function _hasWildcard($str){
+            return (strpos($str, '*') !== false);
+        }
+        
+    }
+    
+}
